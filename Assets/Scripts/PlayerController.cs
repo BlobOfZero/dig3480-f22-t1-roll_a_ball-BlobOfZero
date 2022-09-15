c68d3acfde8ac4158d04234a46b62cfa236bda03
BlobOfZero <111920249+BlobOfZero@users.noreply.github.com>
2022-09-15 17:15:31 +0000

    Sep 15, 2022, 1:15 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -56,6 +56,7 @@ public class PlayerController : MonoBehaviour
 
             SetCountText();
         }
+       
         if(other.gameObject.CompareTag("Enemy"));
         {
             other.gameObject.SetActive(false);
