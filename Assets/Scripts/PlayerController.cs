3674bb2decd2a13db9746f31235b1844e4ce4031
BlobOfZero <111920249+BlobOfZero@users.noreply.github.com>
2022-09-16 14:57:37 +0000

    Sep 16, 2022, 10:57 AM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public TextMeshProUGUI countText;
     public TextMeshProUGUI Lives;
     public GameObject winTextObject;
+    public GameObject loseTextObject;
     private Rigidbody rb;
     private int count;
     private float movementX;
