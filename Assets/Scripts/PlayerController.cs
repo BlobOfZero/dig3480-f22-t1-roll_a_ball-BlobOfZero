93f6f517bea87e78b8c6ad13b4edd6ee99f48ae4
BlobOfZero <111920249+BlobOfZero@users.noreply.github.com>
2022-09-17 20:39:01 +0000

    Sep 17, 2022, 4:39 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -53,7 +53,7 @@ public class PlayerController : MonoBehaviour
         if (lives == 0)
         {
             loseTextObject.SetActive(true);
-            Destroy 
+            Destroy(gameObject);
         }
     }
 
